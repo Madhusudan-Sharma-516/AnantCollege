6d9eb267b0d38f728c9a57ea42649ea61783f93d
Madhusudan Sharma <138996944+Madhusudan-Sharma-516@users.noreply.github.com>
2024-07-15 18:48:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    fontSize: {
+      sm: '0.5rem',
+      'sm2': '0.6rem',
+      am: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+  },
+  plugins: [],
+}
+
